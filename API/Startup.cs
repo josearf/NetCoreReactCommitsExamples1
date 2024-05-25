5dd837c7c37d1f93d63eacdfceb0b1c2631584f8
jose Rodriguez <josearfclass@gmail.com>
2024-05-25 15:39:51 -0700

    NetCore move services to  extension

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,9 +1,4 @@
-using Application.Activities;
-using Application.Core;
-using MediatR;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.OpenApi.Models;
-using Persistence;
+using API.Extensions;
 
 namespace API
 {
@@ -18,21 +13,7 @@ namespace API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
-            });
-            services.AddDbContext<DataContext>(opt => {
-               opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
-            });
-            services.AddCors(opt => {
-               opt.AddPolicy("CorsPolicy", policy =>
-               {
-                    policy.AllowAnyMethod().AllowAnyMethod().WithOrigins("http://localhost:3000");
-               });
-            });
-            services.AddMediatR(typeof(List.Handler).Assembly);
-            services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+            services.AddApplicationServices(_config);
         }
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
