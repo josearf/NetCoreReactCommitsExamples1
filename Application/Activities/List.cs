eaa56a88726e565990341705697da7b0830557c9
jose Rodriguez <josearfclass@gmail.com>
2024-05-25 14:49:20 -0700

    netCore Controllers Create, Details, Edit with Mediator

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -14,7 +14,7 @@ namespace Application.Activities
             private readonly DataContext _context;
             public Handler(DataContext context)
             {
-            _context = context;
+                _context = context;
             }
 
             public Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
