3fbffcd1d0518cc1dc76cab12fc4b59d465898e5
jose Rodriguez <josearfclass@gmail.com>
2024-05-26 09:04:56 -0700

    React Only FrontEnd:  - Create  - Edit  - Delete

--- ReadmeClient.md
+++ ReadmeClient.md
@@ -3,6 +3,9 @@ npm install axios
 # https://react.semantic-ui.com/usage
 npm install semantic-ui-react semantic-ui-css
 
+npm install uuid
+# because uuid don't have typescript definitions
+npm i --save-dev @types/uuid
 
 
 # Help
--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import { Button, Card, CardContent, CardDescription, CardHeader, CardMeta, Icon, Image } from 'semantic-ui-react';
+import { Button, Card, CardContent, CardDescription, CardHeader, CardMeta, Image } from 'semantic-ui-react';
 import { Activity } from '../../../app/models/activity';
 
 interface Props {
     activity: Activity;
     cancelSelectActivity: () => void;
+    openForm: (id: string) => void;
 }
 
 
-export default function ActivityDetails({activity, cancelSelectActivity}: Props) {
+export default function ActivityDetails({activity, cancelSelectActivity, openForm}: Props) {
     return (
         <Card fluid>
             <Image src={`/assets/categoryImages/${activity.category}.jpg`} />
@@ -19,7 +20,7 @@ export default function ActivityDetails({activity, cancelSelectActivity}: Props)
             </CardContent>
             <CardContent extra>
                 <Button.Group widths='2'>
-                    <Button basic color='blue' content='Edit' />
+                    <Button onClick={() => openForm(activity.id)} basic color='blue' content='Edit' />
                     <Button onClick={cancelSelectActivity} basic color='grey' content='Cancel' />
                 </Button.Group>
             </CardContent>
