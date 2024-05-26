4547cb2e0e1734e19c0b20a292463fcb0171a8d4
jose Rodriguez <josearfclass@gmail.com>
2024-05-26 06:54:58 -0700

    React: - Select activity and show in details, - Cancel Remove activity details.

--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -4,10 +4,11 @@ import { Activity } from '../../../app/models/activity';
 
 interface Props {
     activity: Activity;
+    cancelSelectActivity: () => void;
 }
 
 
-export default function ActivityDetails({activity}: Props) {
+export default function ActivityDetails({activity, cancelSelectActivity}: Props) {
     return (
         <Card fluid>
             <Image src={`/assets/categoryImages/${activity.category}.jpg`} />
@@ -19,7 +20,7 @@ export default function ActivityDetails({activity}: Props) {
             <CardContent extra>
                 <Button.Group widths='2'>
                     <Button basic color='blue' content='Edit' />
-                    <Button basic color='grey' content='Cancel' />
+                    <Button onClick={cancelSelectActivity} basic color='grey' content='Cancel' />
                 </Button.Group>
             </CardContent>
         </Card>
