234789857ef5067e08c5df3280c92ac6e9abd61e
jose Rodriguez <josearfclass@gmail.com>
2024-05-27 19:08:58 -0700

    React - Adding Loading on reload page

---
+++ client-app/src/app/layout/LoadingComponent.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Dimmer, Loader } from 'semantic-ui-react';
+
+interface Props {
+    inverted?: boolean;
+    content?: String;
+}
+
+export default function LoadingComponent({inverted = true, content = 'Loading ...'}: Props) {
+    return (
+        <Dimmer active={true} inverted={inverted}>
+            <Loader content={content} />
+        </Dimmer>
+    )
+}
