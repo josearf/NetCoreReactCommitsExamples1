1278721d137a5f64614cf3601bef5c995b37a90b
jose Rodriguez <josearfclass@gmail.com>
2024-05-30 13:27:45 -0700

    React - Mobx Action - Context observable properties

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -1,11 +1,19 @@
-import { makeObservable, observable } from "mobx";
+import { action, makeAutoObservable, makeObservable, observable } from "mobx";
 
 export default class ActivityStore {
     title = 'hello from MobX';
 
     constructor() {
-        makeObservable(this, {
-            title: observable
-        });
+        // makeObservable(this, {
+        //     title: observable,
+        //     setTitle: action
+        // });
+        makeAutoObservable(this);
+    }
+
+    // if this setTitle is an arrow function the it binds directly to the class ActivityStore
+    // if this setTitle is a normal function the inside makeObservable should be setTitle: action.bound
+    setTitle = () => {
+        this.title = this.title + '!';
     }
 }
